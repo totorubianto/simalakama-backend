f21f56b8c2a4da018d08b7c609dc1f61016ffd2d
Toto <toto.rubianto.17@gmail.com>
2019-11-27 09:56:44 +0700

    add env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [
     AuthModule,
     UsersModule,
-    MongooseModule.forRoot('mongodb://LAPTOP-1UN8TADQ:27017/simalakama', {
+    MongooseModule.forRoot(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
       useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
     }),
+    ConfigModule,
   ],
   controllers: [AppController],
   providers: [AppService],
+  
 })
+
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-
+import { ConfigService } from './config/config.service'
 import * as cookieParser from 'cookie-parser';
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
   app.use(cookieParser());
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  await app.listen(3000);
+  app.listen(app.get(ConfigService).getInt('APP_PORT'));
 }
+
 bootstrap();
