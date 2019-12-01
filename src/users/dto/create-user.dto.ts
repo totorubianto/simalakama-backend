5f2302040e21fbab9043cc8d037f4ae259e2f6f7
Toto Rubianto <toto.rubianto.17@gmail.com>
2019-12-01 20:23:46 +0700

    fix bugs

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,21 @@
-import { IsString, IsEmail, IsNumberString, MinLength } from 'class-validator';
+import {
+  IsString,
+  IsEmail,
+  IsNumberString,
+  MinLength,
+  IsNotEmpty,
+} from 'class-validator';
 export class CreateUserDto {
   @IsEmail()
+  @IsNotEmpty()
   readonly email: string;
   @IsString()
   @MinLength(6)
   readonly password: string;
   @IsString()
+  @IsNotEmpty()
   readonly role: string;
   @IsString()
+  @IsNotEmpty()
   readonly name: string;
 }
