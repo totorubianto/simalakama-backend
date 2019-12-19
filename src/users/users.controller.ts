6d1733563d5586349fec9940801321a4bc0476d2
Toto rubianto <toto.rubianto.17@gmail.com>
2019-12-20 05:48:51 +0700

    add request status

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,7 +11,9 @@ import {
   Param,
   Request,
   Headers,
-  BadGatewayException
+  BadGatewayException,
+  HttpCode,
+  BadRequestException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
@@ -31,7 +33,7 @@ import { UpdateForgotPasswordUserDto } from './dto/update-forgot-password.dto';
 import { ForgotPasswordUserDto } from './dto/forgot-password-user.dto';
 import { VerificationService } from '../verification/verification.service';
 import { User } from '../global/decorator/user';
-import { ClientDevice } from '../global/interfaces/client-devices.interface'
+import { ClientDevice } from '../global/interfaces/client-devices.interface';
 
 @Controller('users')
 @UsePipes(ValidationPipe)
@@ -56,10 +58,13 @@ export class UsersController {
     const clientData: ClientDevice = {
       userAgent: userAgent,
       ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
-      deviceToken: deviceToken, 
+      deviceToken: deviceToken,
     };
-    const [register, user, client] = await this.usersService.create(createUserDto,clientData);
-    return {register, user, client}
+    const [register, user, client] = await this.usersService.create(
+      createUserDto,
+      clientData,
+    );
+    return { register, user, client };
   }
   // @Login
   @Post('login')
@@ -69,22 +74,25 @@ export class UsersController {
     @Headers('x-device') device,
     @Headers('x-device-token') deviceToken,
     @Request() req,
-  ) { 
+  ) {
     const clientData: ClientDevice = {
-        userAgent: userAgent,
-        ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
-        deviceToken: deviceToken, 
+      userAgent: userAgent,
+      ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+      deviceToken: deviceToken,
     };
-    
-     const [login, user, client] = await this.usersService.login(loginUserDto, clientData);
-     return {login, user, client}
+
+    const [login, user, client] = await this.usersService.login(
+      loginUserDto,
+      clientData,
+    );
+    return { login, user, client };
   }
 
   // @uUpdate Profile
   @Post('update')
   async update(@Body() updateUserDto: UpdateUserDto, @User() data: any) {
     const user = await this.usersService.updateProfile(updateUserDto, data);
-    return {user}
+    return { user };
   }
 
   //logout
@@ -103,7 +111,7 @@ export class UsersController {
   @Get('find-all')
   async findAll() {
     const users = await this.usersService.findAll(null);
-    return {users};
+    return { users };
   }
 
   // @me
@@ -117,15 +125,17 @@ export class UsersController {
   async requestForgotPassword(
     @Body() forgotPasswordUserDto: ForgotPasswordUserDto,
   ): Promise<any> {
-     const req = await this.usersService.requestForgotPassword(forgotPasswordUserDto);
-     return { message: 'ok' }
+    const req = await this.usersService.requestForgotPassword(
+      forgotPasswordUserDto,
+    );
+    return { message: 'ok' };
   }
 
   // Verify Password
   @Get('verify/:token')
   async verifyForgotPassword(@Param('token') token: string): Promise<any> {
     const verify = await this.verificationService.verify(token);
-    return {message: 'ok'}
+    return { message: 'ok' };
   }
 
   // Forgot Password
@@ -134,8 +144,11 @@ export class UsersController {
     @Param('token') token: string,
     @Body() updateForgotPasswordUserDto: UpdateForgotPasswordUserDto,
   ) {
-     const user = await this.usersService.forgotPassword(updateForgotPasswordUserDto, token);
-     return {user}
+    const user = await this.usersService.forgotPassword(
+      updateForgotPasswordUserDto,
+      token,
+    );
+    return { user };
   }
 
   // uploadAvatar
@@ -151,6 +164,6 @@ export class UsersController {
   )
   async uploadedFile(@UploadedFile() file, @User() userData: any) {
     const user = await this.usersService.uploadAvatar(file, userData);
-    return {user}
+    return { user };
   }
 }
