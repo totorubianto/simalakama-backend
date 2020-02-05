0f7d3878e0e1119498567f6d116e43a2f6e6e4ae
Toto <toto.rubianto.17@gmail.com>
2020-02-05 13:32:36 +0700

    add interface username

--- src/users/interfaces/user.interface.ts
+++ src/users/interfaces/user.interface.ts
@@ -1,9 +1,10 @@
 import { Document } from 'mongoose';
 export interface User extends Document {
-  readonly email: string;
-  readonly firstName: string;
-  readonly lastName: string;
-  readonly password: string;
-  readonly createdAt: string;
-  readonly updatedAt: string;
+    readonly email: string;
+    readonly firstName: string;
+    readonly lastName: string;
+    readonly password: string;
+    readonly slug: string;
+    readonly createdAt: string;
+    readonly updatedAt: string;
 }
